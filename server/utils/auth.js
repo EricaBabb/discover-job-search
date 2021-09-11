768bf9f2b51619e30a7d2c369a07f4e919a80ec5
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-09-11 16:21:10 -0400

    connection  / utils

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,25 +1,22 @@
 const jwt = require('jsonwebtoken');
 
-//Secret allows server to verify
+
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // Function for our authenticated routes
+
   authMiddleware: function({ req, res, next }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token
         .split(' ').pop().trim();
     }
 
-    // IF no token, return request object as is
     if (!token) {
       return res.status(400).json({ message: 'You have no token!' });
     }
-    // Verify the token and get the user data
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
@@ -28,7 +25,6 @@ module.exports = {
       return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // Send to end/break
     next();
   },
   signToken: function({ username, email, _id }) {

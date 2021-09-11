3cc8fb565527931e2dc436ef7901611a2af525c3
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-09-11 11:57:22 -0400

    3rd attempt to commit and push

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,71 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async(parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveJob: async (parent, args, context) => {
+            if (context.user) {
+                const userUpdate = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedJobs: args.job } },
+                    { new: true }
+                );
+
+                return userUpdate;
+            }
+
+            throw new AuthenticationError('Need to be logged in to add a job to your list');
+        },
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const userUpdate = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedJobs: { jobId: args.jobId } } },
+                    { new: true }
+                );
+                
+                return userUpdate;
+            }
+
+            throw new AuthenticationError('Need to be logged in to add a job to your list');
+        } 
+    }
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDef.js
@@ -0,0 +1,44 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Job {
+        _id: ID
+        authors: [String]
+        description: String
+        JobId: String
+        image: String
+        link: String
+        title: String
+    }
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        jobCount: Int
+        savedJobs: [job]
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+    }
+    input jobToSave {
+        authors: [String]
+        description: String
+        jobId: String
+        image: String
+        link: String
+        title: String
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveJob(job: jobToSave): User
+        removeJob(jobId: String!): User
+    }
+`;
+
+module.exports = typeDefs;

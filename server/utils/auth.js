fb0de3c8726fa93208dee5cbbce7cc98a500b47a
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-09-11 14:48:57 -0400

    comp LoginForm

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,24 +1,21 @@
 const jwt = require('jsonwebtoken');
 
-//secret merely enables the server to verify whether it recognizes this token
+//Secret allows server to verify
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
+  // Function for our authenticated routes
   authMiddleware: function({ req, res, next }) {
-    // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token
-        .split(' ')
-        .pop()
-        .trim();
+        .split(' ').pop().trim();
     }
 
-    // if no token, return request object as is
+    // IF no token, return request object as is
     if (!token) {
       return res.status(400).json({ message: 'You have no token!' });
     }
